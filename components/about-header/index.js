cf1fa20dbba38d9f85aadc92923d44cc9d11bde3
Kiet <72509022+wwwKiet@users.noreply.github.com>
2020-05-30 01:19:54 +0300

    github update

--- components/about-header/index.js
+++ components/about-header/index.js
@@ -5,13 +5,12 @@ const AboutHeader = () => (
   <AboutHeaderWrap>
     <div className="container container-small">
       <div className="author-info">
-        <img className='author-img d-none d-sm-none d-md-block d-lg-block d-xl-block' src="/kamran.jpeg" />
+        <img className='author-img d-none d-sm-none d-md-block d-lg-block d-xl-block' src="/artem.jpeg" />
         <div className="author-msg">
           <h1>Hello, I'm Artem Medianyk.</h1>
-          <p>I created <span className='flow-black'>roadmap.sh</span> to help developers find their path if they are confused and help them grow in their career.</p>
+          <p>I created <span className='flow-black'>developer's guide</span> to help developers find their path if they are confused and help them grow in their career.</p>
           <div className="author-links">
-            <a href={`https://twitter.com/${siteConfig.twitter}`} target="_blank">@kamranahmedse</a>
-            <a href="mailto:kamran@roadmap.sh">kamran@roadmap.sh</a>
+            <a href="https://github.com/artyomDev/developer.guide">artyomDev</a>
           </div>
         </div>
       </div>
